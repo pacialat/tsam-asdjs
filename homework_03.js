cefe48a07c291a4341c657d2714770e63efb466e
pacialat <pacialat@gmail.com>
2015-11-22 14:29:06 +0100

    Primo commit es 5

---
+++ homework_03.js
@@ -0,0 +1,56 @@
+/*
+Esercizio 1
+Dato un array di interi, 
+restituire la loro somma fino a che non viene ritrovato un valore negativo
+*/
+
+Array.prototype.sumAllWhilePos = function(){
+    var tot = 0;
+    this.every( x =>{
+        if (x > 0) {
+            tot += x;
+        }
+        return x > 0;
+    });
+    return tot;
+}
+
+Array.prototype.ex_1 = Array.prototype.sumAllWhilePos;
+
+/*
+Esercizio 3
+Dato un array, calcolarne la media
+*/
+
+Array.prototype.average = function (){
+    var tot = 0;
+    this.forEach(x => {
+        tot += x;
+    });
+    return (tot / this.length);
+}
+
+Array.prototype.ex_3 = Array.prototype.average;
+
+
+/*Esercizio 11
+Data una lista di interi A, si riordini gli elementi della lista in modo tale che tutti gli elementi
+dispari precedano nello stesso ordine tutti gli elementi pari.
+*/
+
+Array.prototype.ex_11 = function () {
+    return this.filter(x => x % 2 != 0).concat(this.filter(x => x % 2 == 0));
+}
+
+
+/**/
+function Queue(){
+    this.myArray= [];
+}
+Queue.prototype.enqueue = function (e){
+    this.myArray.unshift(e);
+}
+Queue.prototype.dequeue = function (e){
+    return this.myArray.pop();
+}
+
